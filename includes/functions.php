036f790e83673d1c7794f2853bc467e5c8135eb7
Mohit Bansal <daiict180@gmail.com>
2015-04-06 04:44:57 +0530

    image function added

--- includes/functions.php
+++ includes/functions.php
@@ -134,4 +134,18 @@
 	    }
 	}
 
+	function GetImageExtension($imagetype)
+     {
+       if(empty($imagetype)) return false;
+       switch($imagetype)
+       {
+           case 'image/bmp': return '.bmp';
+           case 'image/gif': return '.gif';
+           case 'image/jpeg': return '.jpg';
+           case 'image/png': return '.png';
+           default: return false;
+       }
+     }
+
+
 ?>
